80c651ecac45d9cbbc4863592731e131ef80fabe
8hEFq5JzQ59XG5oxl2kU <117926493+8hEFq5JzQ59XG5oxl2kU@users.noreply.github.com>
2023-08-06 12:53:21 +0200

    Update antiglobal.lua

--- antiglobal.lua
+++ antiglobal.lua
@@ -1,10 +1,1 @@
-print("a")
-task.wait(1)
-
-print("")
-
-while true do
-    print("")
-    
-    game.Players.LocalPlayer:Kick("Using the source code version of slowpihax is no longer allowed!")
-end
+print("slowpihax will update soon.")
